8056590c5d8cef5cd31a7f4c4254a6ed9b9b4bc2
emt-labor <56870107+EMT-Labor@users.noreply.github.com>
2019-11-11 17:32:53 +0100

    Version 4.0

--- DigitalReadSerial/DigitalReadSerial.ino
+++ DigitalReadSerial/DigitalReadSerial.ino
@@ -9,12 +9,12 @@
 */
 
 // digital pin 2 has a pushbutton attached to it. Give it a name:
-int pushButton = 2;
+int pushButton = 6;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(80000);
+  Serial.begin(90000);
   // make the pushbutton's pin an input:
   pinMode(pushButton, INPUT);
 }
